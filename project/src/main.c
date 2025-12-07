dd9120ff6785b9e5a9dce689e01aa5647800f942
MateusBU <mateusurtassum@gmail.com>
2025-12-07 15:54:59 -0300

    First commit. Code to test threads

---
+++ project/src/main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+
+void* blink_led(void *arg);
+void* read_sensor(void *arg);
+
+
+int main(void) {
+    pthread_t th1, th2;
+
+
+    pthread_create(&th1, NULL, blink_led, NULL);
+
+    pthread_create(&th2, NULL, read_sensor, NULL);
+
+    pthread_join(th1, NULL);
+    pthread_join(th2, NULL);
+
+    return 0;
+}
+
+void* blink_led(void *arg) {
+    printf("Config LED\n");
+
+    for(;;) {
+        printf("LED Blink \n");
+        usleep(1000000);
+    }
+}
+
+void* read_sensor(void *arg) {
+    printf("Config SENSOR\n");
+    
+    for(;;) {
+        printf("Sensor read \n");
+        usleep(700000);
+    }
+}
